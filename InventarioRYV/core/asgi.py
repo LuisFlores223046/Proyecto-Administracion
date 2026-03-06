eed5ee691d2803e09e1296aae4d756812e40dd90
Erick-Rangel-234591 <al234591@alumnos.uacj.mx>
2026-03-05 17:53:27 -0700

    Django project generated, .gitignore, edicion project libre

--- InventarioRYV/core/asgi.py
+++ InventarioRYV/core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Inventario_RYV project.
+ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Inventario_RYV.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
